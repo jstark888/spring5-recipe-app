a0b8d033ec2bfce74f4394aa06eb0d4b079a72ab
Jeff Stark <jstark888@gmail.com>
2021-02-09 16:57:41 -1000

    Renaming UnitOfMeasureRepositoryITest to UnitOfMeasureRepositoryIT. Adding and configuring maven-failsafe-plugin to run integration tests.

--- src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -17,7 +17,7 @@ import static org.junit.Assert.*;
  */
 @RunWith(SpringRunner.class)
 @DataJpaTest
-public class UnitOfMeasureRepositoryITest {
+public class UnitOfMeasureRepositoryIT {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
